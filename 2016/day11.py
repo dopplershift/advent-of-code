b689975d9b4d1a130f3197ab0f25dffbc3eb95f5
Ryan May <rmay@ucar.edu>
2021-11-17 04:06:25 -0700

    Tweak 2016 Day 11
    Re-do the internal layout as a list of floors. This is much more
    efficient and cut about 60% off the run time.

--- 2016/day11.py
+++ 2016/day11.py
@@ -1,68 +1,86 @@
 from collections import deque
-import functools
 from itertools import combinations
-import operator
 import re
+
 
 generator_pattern = re.compile(r'(\w+) generator', re.ASCII)
 microchip_pattern = re.compile(r'(\w+)-compatible microchip', re.ASCII)
 def parse(s):
-    return tuple(frozenset('G' + item[:2] for item in re.findall(generator_pattern, line)) | 
-                 frozenset(item[:2] for item in re.findall(microchip_pattern, line))
-                 for line in s.split('\n'))
+    d = dict()
+    chips = []
+    for floor, line in enumerate(s.split('\n')):
+        for item in re.findall(generator_pattern, line):
+            d['G' + item[:2]] = floor
+        for item in re.findall(microchip_pattern, line):
+            d[item[:2]] = floor
+            chips.append(item[:2])
+    
+    state = [0] * (2 * len(chips))
+    for ind, chip in enumerate(chips):
+        state[2 * ind] = d[chip]
+        state[2 * ind + 1] = d['G' + chip]
+    state.append(len(s.split('\n')))
+        
+    return state
 
 
 def is_safe(state):
-    for floor in state:
-        gens = {item[1:] for item in floor if item[0] == 'G'}
-        if gens:
-            if {item for item in floor if item[0] != 'G'} - gens:
-                return False
-    return True
+    unprotected = set(a for (a, b) in zip(state[::2], state[1::2]) if a != b)
+    gens = set(state[1::2])
+    return not bool(gens & unprotected)
 
 
 def get_grab_options(all_items):
-    all_items = list(all_items)
-    for ind, item in enumerate(all_items):
-        yield {item}
-        is_gen = item[0] == 'G'
-        for item2 in all_items[ind + 1:]:
-            if ((is_gen and (item2[0] == 'G' or item[1:] == item2)) or
-                (not is_gen and (item2[0] != 'G' or item == item2[1:]))):
-                yield {item, item2}
+    elev = all_items[-1]
+    items = [ind for ind, val in enumerate(all_items[:-1]) if val == elev]
+    for ind, item in enumerate(items):
+        yield (item,)
+        is_chip = item % 2 == 0
+        for item2 in items[ind + 1:]:
+            if ((is_chip and (item2 % 2 == 0 or item2 == item + 1)) or
+                (not is_chip and item2 % 2 == 1)):
+                yield item, item2
 
 
-def get_move_options(items, loc, grabbed, cap):
+def get_move_options(items, grabbed, cap):
+    loc = items[-1]
+    new_items = list(items)
     if loc > 0:
         down = loc - 1
-        yield down, items[:down] + (items[down] | grabbed,) + (items[loc] - grabbed,) + items[loc + 1:]
+        new_items[grabbed[0]] = down
+        new_items[grabbed[-1]] = down
+        new_items[-1] = down
+        yield new_items
     if loc < cap:
         up = loc + 1
-        yield up, items[:loc] + (items[loc] - grabbed,) + (items[up] | grabbed,) + items[up + 1:]
-    
+        new_items[grabbed[0]] = up
+        new_items[grabbed[-1]] = up
+        new_items[-1] = up
+        yield new_items
+
 
 def breadth_first_search(start):
-    all_elem = functools.reduce(operator.or_, start)
-    top_floor = len(start) - 1
-    frontier = deque([(start, 0, 0)])
+    top_floor = start[-1] - 1
+    start = list(start)
+    start[-1] = 0
+    frontier = deque([(tuple(start), 0)])
     seen = set()
     while frontier:
-        items, elev, moves = frontier.pop()
-        if items[-1] == all_elem:
+        items, moves = frontier.pop()
+        if all(i == top_floor for i in items[:-1]):
             break
-        elif (items, elev) in seen:
+        elif items in seen:
             continue
         
-        seen.add((items, elev))
-        for taken in get_grab_options(items[elev]):
-            for new_elev, new_items in get_move_options(items, elev, taken, top_floor):
+        seen.add(items)
+        for taken in get_grab_options(items):
+            for new_items in get_move_options(items, taken, top_floor):
                 if is_safe(new_items):
-                    frontier.appendleft((new_items, new_elev, moves + 1))
+                    frontier.appendleft((tuple(new_items), moves + 1))
     else:
         raise RuntimeError('Failed to achieve goal!')
 
     return moves
-
 
 if __name__ == '__main__':
     from aocd.models import Puzzle
@@ -80,6 +98,7 @@ The fourth floor contains nothing relevant.'''
     puz.answer_a = breadth_first_search(layout)
     print('Part 1:', puz.answer_a)
 
-    new_layout = (layout[0] | {'G' + 'el', 'el', 'G' + 'di', 'di'},) + layout[1:]
+    new_layout = parse(puz.input_data)
+    new_layout = new_layout[:-1] + [0, 0, 0, 0] + new_layout[-1:]
     puz.answer_b = breadth_first_search(new_layout)
     print('Part 2:', puz.answer_b)
