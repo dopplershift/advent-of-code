77e7e7f4faa18220f1cc47d2f0cba7d059341504
Ryan May <rmay@ucar.edu>
2021-11-17 04:08:36 -0700

    Significantly speed up 2016 Day 11
    The key (inspired by Reddit) is that the elements don't matter, only how
    the pairs are arranged. So from a solution standpoint,
    [0, 0, 1, 1, 0, 0] is identical to [1, 1, 0, 0, 0, 0]. Account for this
    by creating a canonical representation of the state when checking
    whether we've seen it before. This produces a massive speedup (20x to
    200x depending on which problem).

--- 2016/day11.py
+++ 2016/day11.py
@@ -59,6 +59,10 @@ def get_move_options(items, grabbed, cap):
         yield new_items
 
 
+def canonicalize(state):
+    return tuple(sorted(list(zip(state[::2], state[1::2]))) + [state[-1]])
+
+
 def breadth_first_search(start):
     top_floor = start[-1] - 1
     start = list(start)
@@ -69,10 +73,10 @@ def breadth_first_search(start):
         items, moves = frontier.pop()
         if all(i == top_floor for i in items[:-1]):
             break
-        elif items in seen:
+        elif canonicalize(items) in seen:
             continue
         
-        seen.add(items)
+        seen.add(canonicalize(items))
         for taken in get_grab_options(items):
             for new_items in get_move_options(items, taken, top_floor):
                 if is_safe(new_items):
