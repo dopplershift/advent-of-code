2b534909b567f4df99c287769f00f69bcfd901a8
Ryan May <rmay@ucar.edu>
2021-11-23 02:27:24 -0700

    Fix whitespace issue in 2016 Day 10

--- 2016/day10.py
+++ 2016/day10.py
@@ -41,11 +41,11 @@ if __name__ == '__main__':
     from aocd.models import Puzzle
 
     s = '''value 5 goes to bot 2
-    bot 2 gives low to bot 1 and high to bot 0
-    value 3 goes to bot 1
-    bot 1 gives low to output 1 and high to bot 0
-    bot 0 gives low to output 2 and high to output 0
-    value 2 goes to bot 2'''
+bot 2 gives low to bot 1 and high to bot 0
+value 3 goes to bot 1
+bot 1 gives low to output 1 and high to bot 0
+bot 0 gives low to output 2 and high to output 0
+value 2 goes to bot 2'''
 
     assert run(s) == ({0: 5, 1: 2, 2: 3}, 0)
 
