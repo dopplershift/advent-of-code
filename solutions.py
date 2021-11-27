13baaf73db61caf4055129dde2732e831bc42a72
Ryan May <rmay@ucar.edu>
2021-11-26 23:09:41 -0700

    Setup running aoc command line tool
    Add entrypoint and required packaging boilerplate so that this can be
    pip installed and then run solutions using `aoc`.

---
+++ solutions.py
@@ -0,0 +1,20 @@
+import contextlib
+import io
+from pathlib import Path
+import re
+import sys
+
+@contextlib.contextmanager
+def update_sys_path(path):
+    sys.path.append(path)
+    yield sys.path
+    sys.path.remove(path)
+
+
+def run_solution(year, day, data):
+    output = io.StringIO()
+    sol_path = Path(__file__).parent / f'{year}'
+    with contextlib.redirect_stdout(output), update_sys_path(str(sol_path)):
+        exec(open(sol_path / f'day{day}.py', 'rt').read(), {'__name__': '__main__'})
+    return tuple(res[0] for line in output.getvalue().splitlines()
+                 if (res := re.findall(r'Part \d:\s?(.*)', line)))
