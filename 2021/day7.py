5c3be4ab59ffee18330f5eb99dd5056ebf095fa7
Ryan May <rmay@ucar.edu>
2021-12-07 00:09:36 -0700

    Greatly simplify 2021 Day 7
    Since we don't actually need to location of the minimum, we can just
    wrap min() around a generator expression.

--- 2021/day7.py
+++ 2021/day7.py
@@ -12,27 +12,21 @@ def cost2(target, current):
 
 
 def solve(nums, cost_func=cost1):
-    min_pos = -1
-    min_total = 2**32
-    for i in range(min(nums), max(nums) + 1):
-        total = sum(cost_func(i, n) for n in nums)
-        if total < min_total:
-            min_total = total
-            min_pos = i
-    return min_pos, min_total
+    return min(sum(cost_func(i, n) for n in nums)
+               for i in range(min(nums), max(nums) + 1))
 
 
 if __name__ == '__main__':
     from aocd.models import Puzzle
 
     sample = '16,1,2,0,4,2,7,1,2,14'
-    assert solve(parse(sample)) == (2, 37)
-    assert solve(parse(sample), cost2) == (5, 168)
+    assert solve(parse(sample)) == 37
+    assert solve(parse(sample), cost2) == 168
 
     puz = Puzzle(2021, 7)
 
-    puz.answer_a = solve(parse(puz.input_data))[-1]
+    puz.answer_a = solve(parse(puz.input_data))
     print(f'Part 1: {puz.answer_a}')
 
-    puz.answer_b = solve(parse(puz.input_data), cost2)[-1]
+    puz.answer_b = solve(parse(puz.input_data), cost2)
     print(f'Part 2: {puz.answer_b}')
