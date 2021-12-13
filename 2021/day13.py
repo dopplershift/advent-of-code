a4b2954d3a2ed2114a2932fc9ab406498c2d6caf
Ryan May <rmay@ucar.edu>
2021-12-12 22:25:55 -0700

    2021 AOC Day 13
    This was pretty easy if you grok what the folds do to the math of the
    coordinates. Would have been a bit quicker on part 1 if I had read it
    properly (yet again) and seen the "after 1 fold". Happy, though, that I
    was ready to go with the "decode the text" portion.

---
+++ 2021/day13.py
@@ -0,0 +1,67 @@
+from ast import literal_eval
+import re
+
+
+def parse(s):
+    dots, folds = s.split('\n\n')
+    dots = set(literal_eval(d) for d in dots.split('\n'))
+    folds = [re.search(r'[\w\s]+([xy])=(\d+)', line).groups() for line in folds.split('\n')]
+    return dots, folds
+
+
+def do_folds(dots, folds):
+    for axis, loc in folds:
+        loc = int(loc)
+        if axis == 'x':
+            dots = {(x if x < loc else 2 * loc - x, y) for x, y in dots}
+        else:
+            dots = {(x, y if y < loc else 2 * loc - y) for x, y in dots}
+
+    return dots
+
+
+def display(dots):
+    max_x = max(i[0] for i in dots)
+    max_y = max(i[-1] for i in dots)
+
+    print('\n'.join(''.join('#' if (x, y) in dots else ' ' for x in range(max_x + 1)) for y in range(max_y + 1)))
+
+
+if __name__ == '__main__':
+    from aocd.models import Puzzle
+
+    sample = '''6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5'''
+
+    dots, folds = parse(sample)
+    assert len(do_folds(dots, folds[:1])) == 17
+    assert len(do_folds(dots, folds)) == 16
+
+    puz = Puzzle(2021, 13)
+    dots, folds = parse(puz.input_data)
+
+    puz.answer_a = len(do_folds(dots, folds[:1]))
+    print(f'Part 1: {puz.answer_a}')
+
+    display(do_folds(dots, folds))
+    puz.answer_b = 'RGZLBHFP'
+    print(f'Part 2: {puz.answer_b}')
