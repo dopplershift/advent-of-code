7c01a64e27bbf9bc2a972f25145216442341ff57
Ryan May <rmay@ucar.edu>
2022-12-08 23:24:29 -0700

    Update 2022 Day 8
    Refactor to make a Knot class to encapsulate the movement behavior. It's
    not shorter, but the code reads better.

--- 2022/day9.py
+++ 2022/day9.py
@@ -1,36 +1,55 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Knot:
+    x: int = 0
+    y: int = 0
+
+    @property
+    def loc(self):
+        return (self.x, self.y)
+
+    def move(self, d: str):
+        match d:
+            case 'R':
+                self.x += 1
+            case 'L':
+                self.x -= 1
+            case 'U':
+                self.y += 1
+            case 'D':
+                self.y -= 1
+
+    def follow(self, head: Knot):
+        if (dx := head.x - self.x) > 1:
+            self.x += 1
+            self.y = head.y
+        elif dx < -1:
+            self.x -= 1
+            self.y = head.y
+        elif (dy := head.y - self.y) > 1:
+            self.y += 1
+            self.x = head.x
+        elif dy < -1:
+            self.y -= 1
+            self.x = head.x
+
+
 def parse(data):
     return [(l[0], int(l[2:])) for l in data.split('\n')]
 
 
 def simulate(moves, length):
-    rope = [(0, 0)] * length
-    history = {rope[-1]}
-    dirs = {'R': (1, 0), 'L': (-1, 0), 'U': (0, 1), 'D': (0, -1)}
+    rope = [Knot() for _ in range(length)]
+    history = {rope[-1].loc}
     for d, n in moves:
-        dx, dy = dirs[d]
-        for i in range(n):
-            newrope = [(rope[0][0] + dx, rope[0][1] + dy)]
-            for knot in rope[1:]:
-                newrope.append(follow(newrope[-1], knot))
-            rope = newrope
-            history.add(rope[-1])
+        for _ in range(n):
+            rope[0].move(d)
+            for i, knot in enumerate(rope[1:]):
+                knot.follow(rope[i])
+            history.add(rope[-1].loc)
     return history
-
-
-def follow(head, tail):
-    dx = head[0] - tail[0]
-    dy = head[1] - tail[1]
-    mx = my = 0
-    if abs(dx) > 1:
-        mx = 1 if dx > 0 else -1
-        if dy:
-            my = 1 if dy > 0 else -1
-    elif abs(dy) > 1:
-        my = 1 if dy > 0 else -1
-        if dx:
-            mx = 1 if dx > 0 else -1
-
-    return (tail[0] + mx, tail[1] + my)
 
 
 def run(data):
